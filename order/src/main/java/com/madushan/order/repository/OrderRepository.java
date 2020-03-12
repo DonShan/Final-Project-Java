70ccaca263fe2004091fcb17c56bbc7122d3985f
delegate <msdjshan47@gmail.com>
2020-03-12 21:59:32 +0530

    Add all rest controlls

--- order/src/main/java/com/madushan/order/repository/OrderRepository.java
+++ order/src/main/java/com/madushan/order/repository/OrderRepository.java
@@ -4,11 +4,13 @@ import com.madushan.order.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
    Order findByOrdername(String oderName);
 
-   Order findByiD(Integer orderNo);
+   Optional<Order> findById(Integer orderNo);
 
 }
